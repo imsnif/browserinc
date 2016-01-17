267b77a58b1a71fb7819e634215d34f800df8a5f
Aram Drevekenin <grimsniffer@gmail.com>
2016-01-17 23:52:44 +0200

    Initial commit

---
+++ dist/cmd.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+'use strict';
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _readPkg = require('./read-pkg');
+
+var _readPkg2 = _interopRequireDefault(_readPkg);
+
+var _adjustBrowserField = require('./adjust-browser-field');
+
+var _adjustBrowserField2 = _interopRequireDefault(_adjustBrowserField);
+
+var _writeChanges = require('./write-changes');
+
+var _writeChanges2 = _interopRequireDefault(_writeChanges);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function getPkgFromDir(dir) {
+  _fs2.default.readdir(dir, function (err, files) {
+    if (err) throw err;
+    files.forEach(function (file) {
+      if (file === "package.json") return readPkg.write(dir + '/' + file);
+      _fs2.default.stat(dir + '/' + file, function (err, stats) {
+        if (err) throw err;
+        if (stats.isDirectory()) getPkgFromDir(dir + '/' + file);
+      });
+    });
+  });
+}
+
+var buf = _fs2.default.readFileSync(process.cwd() + '/package.json');
+var pkg = JSON.parse(buf.toString());
+if (!pkg.debrowserify.removes) throw new Error("Could not find list of removes in package.json");
+var readPkg = new _readPkg2.default();
+var adjustBf = new _adjustBrowserField2.default(pkg.debrowserify.removes);
+var writeChanges = new _writeChanges2.default();
+readPkg.pipe(adjustBf).pipe(writeChanges);
+getPkgFromDir(process.cwd() + '/node_modules');
---
+++ dist/read-pkg.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _stream = require('stream');
+
+var _fs = require('fs');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ReadPkg = function (_Transform) {
+  _inherits(ReadPkg, _Transform);
+
+  function ReadPkg() {
+    var options = arguments.length <= 0 || arguments[0] === undefined ? { objectMode: true } : arguments[0];
+
+    _classCallCheck(this, ReadPkg);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(ReadPkg).call(this, options));
+  }
+
+  _createClass(ReadPkg, [{
+    key: '_transform',
+    value: function _transform(filename, err, cb) {
+      var _this2 = this;
+
+      (0, _fs.readFile)(filename, function (err, buf) {
+        if (err) throw err;
+        _this2.push({ filename: filename, parsed: JSON.parse(buf.toString()) });
+        cb();
+      });
+    }
+  }]);
+
+  return ReadPkg;
+}(_stream.Transform);
+
+exports.default = ReadPkg;
