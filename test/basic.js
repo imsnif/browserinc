60e25a2ede2ea2fc515a31c066149e3a11d282f0
Aram Drevekenin <grimsniffer@gmail.com>
2016-01-19 19:21:34 +0200

    Refactoring

--- test/basic.js
+++ test/basic.js
@@ -1,42 +1,6 @@
 import test from 'tape'
-import fs from 'fs'
 import path from 'path'
-import find from 'findit'
 import tutils from './utils/test-utils'
-import { fork } from 'child_process'
-
-function spawnCmd (cwd) {
-  let cmd = path.join(__dirname, "..", "dist", "cmd.js")
-  return new Promise((resolve, reject) => {
-    let browserinc = fork(cmd, {cwd, silent: true})
-    browserinc.stderr.on("error", (err) => {
-      browserinc.kill()
-      reject(err)
-    })
-    browserinc.on("exit", (exitCode) => {
-      resolve(cwd)
-    })
-  })
-}
-
-function findOccurrences(mod, cwd) {
-  return new Promise((resolve, reject) => {
-    let occurrences = 0
-    let finder = find(cwd)
-    finder.on("file", (file) => {
-      if (/package\.json$/.test(file)) {
-        fs.readFile(file, (err, contents) => {
-          if (err) reject(err)
-          let pkg = JSON.parse(contents)
-          if (pkg.browser && pkg.browser[mod]) occurrences += 1
-        })
-      }
-    })
-    finder.on("end", () => {
-      resolve(occurrences)
-    })
-  })
-}
 
 test("Include excluded files", function(t) {
   let timer = setTimeout(() => {
@@ -44,8 +8,8 @@ test("Include excluded files", function(t) {
   },5000)
   let cwd = path.join(__dirname, "fixtures", "case1")
   tutils.prepareCase(cwd)
-    .then(spawnCmd)
-    .then(findOccurrences.bind(this, "foo"))
+    .then(tutils.spawnCmd)
+    .then(tutils.findOccurrences.bind(this, "foo"))
     .then((occurrences) => {
       clearTimeout(timer)
       t.equal(occurrences, 0)
--- test/utils/test-utils.js
+++ test/utils/test-utils.js
@@ -1,6 +1,9 @@
 import tmp from 'tmp'
 import { ncp } from 'ncp'
 import fs from 'fs'
+import path from 'path'
+import find from 'findit'
+import { fork } from 'child_process'
 
 export default {
   prepareCase: function (dirName) {
@@ -12,6 +15,37 @@ export default {
         })  
       })  
     })  
+  },
+  spawnCmd: function (cwd) {
+    let cmd = path.join(__dirname, "..", "dist", "cmd.js")
+    return new Promise((resolve, reject) => {
+      let browserinc = fork(cmd, {cwd, silent: true})
+      browserinc.stderr.on("error", (err) => {
+        browserinc.kill()
+        reject(err)
+      })
+      browserinc.on("exit", (exitCode) => {
+        resolve(cwd)
+      })
+    })
+  },
+  findOccurrences: function (mod, cwd) {
+    return new Promise((resolve, reject) => {
+      let occurrences = 0
+      let finder = find(cwd)
+      finder.on("file", (file) => {
+        if (/package\.json$/.test(file)) {
+          fs.readFile(file, (err, contents) => {
+            if (err) reject(err)
+            let pkg = JSON.parse(contents)
+            if (pkg.browser && pkg.browser[mod]) occurrences += 1
+          })
+        }
+      })
+      finder.on("end", () => {
+        resolve(occurrences)
+      })
+    })
   }
 }
 
